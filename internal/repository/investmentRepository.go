a2c98ac0935b77ca96c66be6db916797c44cf446
Phongsakorn Tangpok <664259011@webmail.npru.ac.th>
2026-03-27 15:37:10 +0700

    add investment & transaction in repository folder

--- internal/repository/investmentRepository.go
+++ internal/repository/investmentRepository.go
@@ -6,26 +6,74 @@ import (
 	"gorm.io/gorm"
 )
 
-// interface
-
 type InvestmentRepository interface {
 	Create(investment *domain.Investment) error
+	FindByID(id uint) (*domain.Investment, error)
+	FindByReferenceNumber(ref string) (*domain.Investment, error)
+	UpdateStatus(id uint, status domain.InvestmentStatus) error
+	UpdatePaid(investment *domain.InvestmentStatus) error
+	ListByBoosterUserID(boosterUserID uint) ([]domain.Investment, error)
+	GetFundingConfig(projectID uint) (*domain.Project, error)
 }
-
-// struct
 
 type investmentRepository struct {
 	db *gorm.DB
 }
 
-// constructors
-
 func NewInvestmentRepository(db *gorm.DB) InvestmentRepository {
 	return &investmentRepository{db}
 }
 
-// investmentrepository methods
-
 func (r *investmentRepository) Create(investment *domain.Investment) error {
 	return r.db.Create(investment).Error
+}
+
+func (r *investmentRepository) FindByID(id uint) (*domain.Investment, error) {
+	inv := &domain.Investment{}
+
+	err := r.db.First(inv, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return inv, nil
+}
+
+func (r *investmentRepository) FindByReferenceNumber(ref string) (*domain.Investment, error) {
+	inv := &domain.Investment{}
+
+	err := r.db.Where("reference_number = ?", ref).First(inv).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return inv, nil
+}
+
+func (r *investmentRepository) UpdateStatus(id uint, status domain.InvestmentStatus) error {
+	return r.db.Model(&domain.Investment{}).Where("id = ?", id).Update("status", status).Error
+}
+
+func (r *investmentRepository) UpdatePaid(investment *domain.InvestmentStatus) error {
+	return r.db.Save(investment).Error
+}
+
+func (r *investmentRepository) ListByBoosterUserID(boosterUserID uint) ([]domain.Investment, error) {
+	inv := []domain.Investment{}
+
+	err := r.db.Where("booster_user_id = ?", boosterUserID).Order("created_at desc").Find(&inv).Error
+
+	return inv, err
+}
+
+func (r *investmentRepository) GetFundingConfig(projectID uint) (*domain.Project, error) {
+	config := &domain.Project{}
+
+	err := r.db.Where("project_id = ?", projectID).First(config).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
 }
--- internal/repository/transactionRepository.go
+++ internal/repository/transactionRepository.go
@@ -6,10 +6,11 @@ import (
 	"gorm.io/gorm"
 )
 
-// interface
-
 type TransactionRepository interface {
 	Create(txn *domain.Transaction) error
+	FindByPaymentIntentID(intentID string) (*domain.Transaction, error)
+	FindByInvestmentID(investmentID uint) (*domain.Transaction, error)
+	UpdateStatus(id uint, status domain.TransactionStatus) error
 }
 
 type transactionRepository struct {
@@ -22,4 +23,32 @@ func NewTransactionRepository(db *gorm.DB) TransactionRepository {
 
 func (r *transactionRepository) Create(txn *domain.Transaction) error {
 	return r.db.Create(txn).Error
+}
+
+func (r *transactionRepository) FindByPaymentIntentID(intentID string) (*domain.Transaction, error) {
+	txn := domain.Transaction{}
+
+	err := r.db.Preload("Investment").Where("stripe_payment_intent_id = ?", intentID).First(&txn).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &txn, nil
+}
+
+func (r *transactionRepository) FindByInvestmentID(investmentID uint) (*domain.Transaction, error) {
+	txn := domain.Transaction{}
+
+	err := r.db.Where("investment_id = ?", investmentID).First(&txn).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &txn, nil
+}
+
+func (r *transactionRepository) UpdateStatus(id uint, status domain.TransactionStatus) error {
+	return r.db.Model(&domain.Transaction{}).Where("id = ?", id).Update("status", status).Error
 }
