abb49ee72c462d8ceae0bc6dc4be7ecef973d33f
SundayYogurt <kritsanalalu5@gmail.com>
2026-04-14 00:27:32 +0700

    edit: validate signing

--- internal/service/user.service_test.go
+++ internal/service/user.service_test.go
@@ -24,6 +24,11 @@ type mockUserRepository struct {
 	mock.Mock
 }
 
+func (m *mockUserRepository) FindUserByEmail(email string) (*domain.User, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
 func (m *mockUserRepository) FindStudentRequest(status string) ([]domain.StudentCardVerification, error) {
 	args := m.Called(status)
 
